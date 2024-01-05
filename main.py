b2de9bdb78f439fc84b1ca4628c64d00905811b1
John Rain55 <pasternak146@gmail.com>
2024-01-05 02:08:03 +0100

    added icon image & PhotoImage

--- main.py
+++ main.py
@@ -8,13 +8,22 @@ root.config(bg="dark red")
 root.resizable(False, False)
 
 
+image_icon = PhotoImage(file="icon.png")
+root.iconphoto(False, image_icon)
+
+
 def generate():
     name = title.get()
     text = entry.get()
     qr = qrcode.make(text)
     qr.save("Qrcode/" + str(name) + ".png")
+    global Image
+    Image = PhotoImage(file="Qrcode/" + str(name) + ".png")
+    Image_view.config(image=Image)
 
 
+Image_view = Label(root, bg="dark red")
+Image_view.pack(padx=50, pady=10, side=RIGHT)
 Label(root, text="Title", fg="white", bg="dark red", font=15).place(x=50, y=170)
 
 title = Entry(root, width=13, font="arial 15")
@@ -23,5 +32,5 @@ title.place(x=50, y=200)
 entry = Entry(root, width=28, font="arial 15")
 entry.place(x=50, y=250)
 
-Button(root, text="Generate", width=20, height=2, bg="white", fg="red", command=generate).place(x=50, y=300)
+Button(root, text="Generate", width=20, height=2, bg="white", fg="dark red", command=generate).place(x=50, y=300)
 root.mainloop()
